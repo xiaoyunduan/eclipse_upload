01f6de9043fcc40d983f9b537ce686eadb6e4b90
xiaoyunduan <xiaoyunduang@163.com>
2018-03-08 21:59:04 +0800

    commit

---
+++ src/mytool/GetImage.java
@@ -0,0 +1,22 @@
+package mytool;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+public class GetImage {
+ 
+	public static Image getImage(String path){
+		URL u=GetImage.class.getClassLoader().getResource(path);
+		BufferedImage img=null;
+		try{
+			img=ImageIO.read(u);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		return img;
+	}
+}
